d4aa89b381efba495d7337daad5cb3a37868ef53
unknown <181034>
2021-11-17 14:43:26 +0900

    first

---
+++ src/main/java/com/example/online_seminar/OnlineSeminarApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_seminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSeminarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineSeminarApplication.class, args);
+    }
+
+}
