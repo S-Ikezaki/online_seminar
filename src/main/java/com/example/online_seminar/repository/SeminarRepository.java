8f2b9171e1bc628cc2331e2fa7512c060899c057
NakanoMasahiro <181016@std.hi-joho.ac.jp>
2021-11-19 11:58:38 +0900

    Revert "DB connection"
    This reverts commit 252768706e5336c84bdc703870eebf33c863ec31.

--- src/main/java/com/example/online_seminar/repository/SeminarRepository.java
+++ src/main/java/com/example/online_seminar/repository/SeminarRepository.java
@@ -1,9 +1,7 @@
 package com.example.online_seminar.repository;
 
-import com.example.online_seminar.entity.GroupMst;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface SeminarRepository extends JpaRepository<GroupMst, Long> {
+public interface SeminarRepository extends JpaRepository {
+
 }
