252768706e5336c84bdc703870eebf33c863ec31
NakanoMasahiro <181016@std.hi-joho.ac.jp>
2021-11-19 11:55:51 +0900

    DB connection
    IDEAでDBに接続するために必要なデータ

--- src/main/java/com/example/online_seminar/repository/SeminarRepository.java
+++ src/main/java/com/example/online_seminar/repository/SeminarRepository.java
@@ -1,7 +1,9 @@
 package com.example.online_seminar.repository;
 
+import com.example.online_seminar.entity.GroupMst;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SeminarRepository extends JpaRepository {
-
+@Repository
+public interface SeminarRepository extends JpaRepository<GroupMst, Long> {
 }
