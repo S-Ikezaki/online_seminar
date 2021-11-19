87d6a8c3ad85d4cb5d8f9f608a5d45fbf9c6d15b
akatadaisuki <181005@std.hi-joho.ac.jp>
2021-11-19 11:45:04 +0900

    ログイン画面仮完成
    ログイン画面の見た目だけ完成しました（何打ってもログイン後画面に遷移する）
    HomeControllerを修正しました
    Certificationを修正しました
    application.propertiesに加筆しました（エラーメッセージ表示のため）
    login.htmlを修正しました
    mainmenuを作成しました
    ValidationMessage.propertiesを作成しました

--- src/main/java/com/example/online_seminar/controller/HomeController.java
+++ src/main/java/com/example/online_seminar/controller/HomeController.java
@@ -1,16 +1,38 @@
 package com.example.online_seminar.controller;
 
+import com.example.online_seminar.model.Certification;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
-//    @GetMapping("/login"){
-//        return "login";
-//    }
+
+    @GetMapping("/")
+    public String login(@ModelAttribute Certification certification) {
+        return "login";
+    }
+
+
+    @PostMapping("/")
+    public String meinmenu(@Validated @ModelAttribute Certification certification, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "login";
+        }
+        return "mainmenu";
+    }
+}
+
+
+
+
+
 //
 //    @PostMapping
 //
 //}
-}
+
