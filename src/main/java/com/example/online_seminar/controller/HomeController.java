931d605f32c3e9545781c868cf6a5fa0da0fd9a1
ooguikarateka <181033@std.hi-joho.ac.jp>
2021-11-24 12:31:15 +0900

    ホームコントローラー編集
    ホームコントローラーをきれいにしました

--- src/main/java/com/example/online_seminar/controller/HomeController.java
+++ src/main/java/com/example/online_seminar/controller/HomeController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //package com.example.online_seminar.controller;
 //
 //import com.example.online_seminar.model.Certification;
@@ -8,7 +7,6 @@
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.ModelAttribute;
 //import org.springframework.web.bind.annotation.PostMapping;
-=======
 package com.example.online_seminar.controller;
 
 import com.example.online_seminar.model.user.Certification;
@@ -42,7 +40,7 @@ public class HomeController {
 
 
 
->>>>>>> e0e7db5fa17b732b65ebd7219b17ebded4b549fb
+//>>>>>>> e0e7db5fa17b732b65ebd7219b17ebded4b549fb
 //
 //@Controller
 //public class HomeController {
