aed6029a80909491d1e0bc0c91927c4ca7dcbfa7
ooguikarateka <181033@std.hi-joho.ac.jp>
2021-11-24 12:30:47 +0900

    userRepository編集
    ユーザーリポジトリをきれいにしました

--- src/main/java/com/example/online_seminar/repository/UserRepository.java
+++ src/main/java/com/example/online_seminar/repository/UserRepository.java
@@ -1,14 +1,6 @@
 package com.example.online_seminar.repository;
-
-<<<<<<< HEAD
-import com.example.online_seminar.model.Certification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Certification,Integer> {
-=======
 import com.example.online_seminar.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
->>>>>>> e0e7db5fa17b732b65ebd7219b17ebded4b549fb
 }
