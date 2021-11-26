af628e2032c301c1b36aa39baf498622590b476f
ikezaki <181034@std.hi-joho.ac.jp>
2021-11-26 10:41:35 +0900

    UserRepository編集
    extendsを書き足した

--- src/main/java/com/example/online_seminar/repository/UserRepository.java
+++ src/main/java/com/example/online_seminar/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.example.online_seminar.repository;
+
 import com.example.online_seminar.entity.user.User;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository  {
+public interface UserRepository extends JpaRepository<User,Long> {
     //userテーブルにデータを1件insert
     public int insertOne(User user) throws DataAccessException;
+
+
 }
