4e9e9f99d171174544dfe61ffef7f74aafdc15c2
ooguikarateka <181033@std.hi-joho.ac.jp>
2021-12-01 10:31:09 +0900

    クエリをネイティブクエリで記述
    タグでユーザーを検索するクエリをネイティブクエリで記入しました。ネイティブクエリの変数名の記述規則はプロパティファイルにある通りにしてください

--- src/main/java/com/example/online_seminar/repository/UserRepository.java
+++ src/main/java/com/example/online_seminar/repository/UserRepository.java
@@ -3,10 +3,16 @@ package com.example.online_seminar.repository;
 import com.example.online_seminar.entity.group.Group;
 import com.example.online_seminar.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface UserRepository  extends JpaRepository<User,Long> {
     //グループで検索するメソッド
     List<User> findByGroup(Group group);
+
+    //タグでユーザーを検索するメソッド
+    @Query(name = "User.searchByTagNqVariable" ,nativeQuery = true)
+    Collection<User> searchByTagNqVariable(String tag);
 }
