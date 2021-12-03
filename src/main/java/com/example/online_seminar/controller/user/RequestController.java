a26562ae67f06db358a939c07bd3fa1331c1a8fd
ikezaki <181034@std.hi-joho.ac.jp>
2021-12-03 09:40:23 +0900

    RequestControllerの作成
    Request用のControllerを作成しました

---
+++ src/main/java/com/example/online_seminar/controller/user/RequestController.java
@@ -0,0 +1,7 @@
+package com.example.online_seminar.controller.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("request")
+public class RequestController {
+}
