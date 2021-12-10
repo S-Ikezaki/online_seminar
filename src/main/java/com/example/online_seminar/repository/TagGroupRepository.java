9cde8d3b41b27789e910037bdd89d7c1884953a4
ooguikarateka <181033@std.hi-joho.ac.jp>
2021-12-10 14:40:55 +0900

    タググループリポジトリ編集
    タググループリポジトリを複合主キーに対応させました

--- src/main/java/com/example/online_seminar/repository/TagGroupRepository.java
+++ src/main/java/com/example/online_seminar/repository/TagGroupRepository.java
@@ -1,7 +1,8 @@
 package com.example.online_seminar.repository;
 
 import com.example.online_seminar.entity.tag.TagGroup;
+import com.example.online_seminar.key.TagGroupKeys;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagGroupRepository extends JpaRepository<TagGroup,Long> {
+public interface TagGroupRepository extends JpaRepository<TagGroup,TagGroupKeys> {
 }
