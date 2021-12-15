d26093c7e94297a7ead42455a959ff00960a70f2
akatadaisuki <181005@std.hi-joho.ac.jp>
2021-12-15 10:03:26 +0900

    セキュリティ編集
    SecurityConfig
    SecurityControllerを編集しました

--- src/main/java/com/example/online_seminar/controller/SecurityController.java
+++ src/main/java/com/example/online_seminar/controller/SecurityController.java
@@ -1,24 +1,49 @@
 package com.example.online_seminar.controller;
 
+import com.example.online_seminar.entity.group.Group;
+import com.example.online_seminar.repository.GroupRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class SecurityController {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
+    private final GroupRepository groupRepository;
 
+    @GetMapping("/login")
+    public String login() { return "login"; }
 
     @GetMapping("/")
-    public String showMenu(Authentication loginUser, Model model){
-        model.addAttribute("username",loginUser.getName());
-        model.addAttribute("role",loginUser.getAuthorities());
-        return "user";
-    }
+    public String showMenu(Authentication loginUser, Model model) {
 
-}
+        List<Group> groupList = groupRepository.findAll();
+        List<Group> seminar = new ArrayList<Group>();
+        List<Group> competition = new ArrayList<Group>();
+
+        for (Group group: groupList){
+            if(group.getGroupRole() == 0) {
+                seminar.add(group);
+            } else {
+                competition.add(group);
+            }
+        }
+
+        model.addAttribute("username", loginUser.getName());
+        model.addAttribute("role", loginUser.getAuthorities());
+
+
+        System.out.println(loginUser.getAuthorities());
+        model.addAttribute("groups",groupRepository.findAll());
+
+        model.addAttribute("seminars",seminar);
+        model.addAttribute("competitions",competition);
+
+        return "main_menu";
+    }
