4a5413ebe45a3737cfae36eb5a36adf61a461011
ooguikarateka <181033@std.hi-joho.ac.jp>
2021-12-22 12:24:00 +0900

    コメントアウト削除

--- src/main/java/com/example/online_seminar/entity/group/GroupMessage.java
+++ src/main/java/com/example/online_seminar/entity/group/GroupMessage.java
@@ -2,8 +2,6 @@ package com.example.online_seminar.entity.group;
 
 import com.example.online_seminar.entity.user.User;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
