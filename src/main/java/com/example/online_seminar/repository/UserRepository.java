43608feb7092c2416960ede082fb8544ee189679
ooguikarateka <181033@std.hi-joho.ac.jp>
2022-01-12 13:31:48 +0900

    メンバー一覧表示のためのコントローラー、リポジトリ

--- src/main/java/com/example/online_seminar/repository/UserRepository.java
+++ src/main/java/com/example/online_seminar/repository/UserRepository.java
@@ -50,6 +50,4 @@ public interface UserRepository extends JpaRepository<User,Long> {
     @Query(name = "User.findEmployeeByRoleNq", nativeQuery = true)
     List<User> findEmployeeByRole(String userRole);
 
-    @Query(name = "User.findByGroup",nativeQuery = true)
-    List<User>  findByGroup(String groupId);
 }
