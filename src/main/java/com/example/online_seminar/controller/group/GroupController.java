17cebc999bf846c58770abb1e54a81557fbd0194
ooguikarateka <181033@std.hi-joho.ac.jp>
2022-01-28 12:31:48 +0900

    グループ作成のページ遷移作成

--- src/main/java/com/example/online_seminar/controller/group/GroupController.java
+++ src/main/java/com/example/online_seminar/controller/group/GroupController.java
@@ -316,6 +316,7 @@ public class GroupController {
         return "group_add_complete";
     }
 
+    //グループ作成
     @GetMapping("/teacher/showCreateMenu")
         public String showCreateMenu(Model model){ return "group_add"; }
 
