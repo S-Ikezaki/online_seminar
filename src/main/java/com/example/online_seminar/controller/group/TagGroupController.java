864d8cb2dec6e6d05b487985e8fcc924689170b0
ooguikarateka <181033@std.hi-joho.ac.jp>
2022-02-04 11:29:49 +0900

    グループメンバ表示

--- src/main/java/com/example/online_seminar/controller/group/TagGroupController.java
+++ src/main/java/com/example/online_seminar/controller/group/TagGroupController.java
@@ -2,13 +2,32 @@ package com.example.online_seminar.controller.group;
 
 import com.example.online_seminar.repository.GroupRepository;
 import com.example.online_seminar.repository.TagGroupRepository;
+import com.example.online_seminar.repository.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-//使うかわからない
+@Controller
+@RequestMapping("tags")
 public class TagGroupController {
 
-    @Autowired
-    GroupRepository groupRepository;
-    @Autowired
-    TagGroupRepository tagGroupRepository;
+    final GroupRepository groupRepository;
+    final TagGroupRepository tagGroupRepository;
+    final TagRepository tagRepository;
+
+    public TagGroupController(GroupRepository groupRepository, TagGroupRepository tagGroupRepository,TagRepository tagRepository) {
+        this.groupRepository = groupRepository;
+        this.tagGroupRepository = tagGroupRepository;
+        this.tagRepository = tagRepository;
+    }
+
+    //グループを作成した際に新しいタグを追加、すでにあるタグを持ってくるメソッド
+    @PostMapping("/addTag")
+    public String addTag(){
+
+
+        return "hoge";
+    }
+
 }
